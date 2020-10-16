30fc9fb1bc10c798336578f15f6fbf63608dcdcb
sonrad10 <alaric.whitehead@gmail.com>
2020-10-16 16:59:41 +0100

    Added support for viewing document on other devices on the same network

--- serve.js
+++ serve.js
@@ -2,9 +2,33 @@
 import choikdar from "chokidar";
 import express from "express";
 import listen from "socket.io";
+import os from "os";
 import path from "path";
 import portfinder from "portfinder";
 import { exec } from "child_process";
+
+/**
+ * Return an array of all the IPv4 addresses of this computer
+ * @returns {[string]}	Array of IPv4 addresses
+ */
+function getAllIps() {
+	//Hold the found IPs
+	let addresses = [];
+	//Get all the connected interfaces, stored as { key: [addressInfo,addressInfo,...] }
+	let interfaces = os.networkInterfaces();
+
+	//Iterate over the interfaces
+	for (let intfc of Object.values(interfaces)) {
+		//Iterate over the addresses
+		for (let addrInfo of intfc) {
+			//Check the address is IPv4 and externally accessible
+			if (addrInfo.family === 'IPv4' && !addrInfo.internal && addrInfo.address) {
+				addresses.push(addrInfo.address);
+			}
+		}
+	}
+	return addresses;
+}
 
 // ================
 
@@ -36,16 +60,22 @@ function getRenderedData(callback) {
 
 // ================
 
+//Get the first external IP address in the list
+const externalIp = getAllIps()[0] || null;
 
 //Get a free port to use
-portfinder.getPort({ port: 7000 }, (err, port) => {
+portfinder.getPort({ port: 7000, host: externalIp }, (err, port) => {
+	//Convert `localhost` and the external IP to URLs
+	const localAddress = `http://localhost:${port}`;
+	const externalAddress = externalIp ? `http://${externalIp}:${port}` : null;
+
 	//HTML/JS to run on the client side
 	const RESPONSE_HTML = `
 	<!--The socket.io library-->
 	<script src="/socket.io/socket.io.js"></script>
 	<script>
 		//Establish a socket connection to the server
-		const socket = io.connect('http://localhost:${port}');
+		const socket = io.connect('${externalAddress || localAddress}');
 		//Display the new HTML when it is received
 		socket.on('updated', function(data) {
 			document.getElementById("body").innerHTML = data;
@@ -57,7 +87,13 @@ portfinder.getPort({ port: 7000 }, (err, port) => {
 
 	//Create a web server to listen on the chosen port
 	const app = express();
-	const server = app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+	const server = app.listen(port, () => {
+		console.log("========");
+		console.log(`App running at:`);
+		console.log(`- Local:\t${localAddress}`);
+		if (externalAddress) console.log(`- Network:\t${externalAddress}`);
+		console.log("========");
+	});
 	//Allow socket connections on the same port
 	const socketIo = listen(server);
 
@@ -65,7 +101,7 @@ portfinder.getPort({ port: 7000 }, (err, port) => {
 	// Source: https://stackoverflow.com/a/18311469/2966288
 	app.use(function (req, res, next) {
 		//Allow connecting from the client only
-		res.setHeader('Access-Control-Allow-Origin', `http://localhost:${port}`);
+		res.setHeader('Access-Control-Allow-Origin', `*`);
 		//Request methods to allow
 		//'GET, POST, OPTIONS, PUT, PATCH, DELETE'
 		res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
