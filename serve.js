347d9cf3d49b2a9ff1678d2ea934e0d78a55948e
sonrad10 <alaric.whitehead@gmail.com>
2020-11-06 10:46:53 +0000

    Added version number

--- serve.js
+++ serve.js
@@ -38,6 +38,7 @@ function getAllIps() {
 //Build the command line argument parser
 const program = new Command.Command()
 	.name("asciidoctor-serve")
+	.version("1.1.0")
 	.usage("[options] [asciidoctor options]")
 	.allowUnknownOption()
 	.option('-pdf', "Whether to serve a PDF viewer instead of a web server")
